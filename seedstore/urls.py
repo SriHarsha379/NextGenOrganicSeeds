6c02690fa0aac3a330e4039e3e36ddf2f0ed9c7f
unknown <harshas379@gmail.com>
2025-04-30 11:48:31 +0530

    new images

--- seedstore/urls.py
+++ seedstore/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users.views import home_redirect  # Import the home redirect view
 from cart.views import order_success
 
@@ -12,3 +14,6 @@ urlpatterns = [
     path('order-success/', order_success, name='order_success'),
 ]
 
+# ✅ Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
