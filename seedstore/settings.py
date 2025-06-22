a5a92a1b8aa5edabf64026dc4adc9f03a78c3831
Sri Harsha <harshas379@gmail.com>
2025-06-22 12:34:14 +0000

    Updated settings, requirements, seeds data; added maintenance page

--- seedstore/settings.py
+++ seedstore/settings.py
@@ -17,7 +17,7 @@ from decouple import Csv, Config, RepositoryEnv
 
 
 from pathlib import Path
-from decouple import config
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
